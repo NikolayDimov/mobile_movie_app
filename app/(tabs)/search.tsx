e43044893cb8cc8abe835ca4631512b83c917cd4
Nikolay Dimov <dimovv.nikolay@gmail.com>
2025-03-19 17:35:36 +0200

    fix(search) displaying searched movies

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -27,11 +27,6 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
-
-                // Call updateSearchCount only if there are results
-                if (movies?.length! > 0 && movies?.[0]) {
-                    await updateSearchCount(searchQuery, movies[0]);
-                }
             } else {
                 reset();
             }
@@ -39,6 +34,13 @@ const Search = () => {
 
         return () => clearTimeout(timeoutId);
     }, [searchQuery]);
+
+    useEffect(() => {
+        // Call updateSearchCount only if there are results
+        if (movies?.length! > 0 && movies?.[0]) {
+            updateSearchCount(searchQuery, movies[0]);
+        }
+    }, [movies])
 
     return (
         <View className="flex-1 bg-primary">
